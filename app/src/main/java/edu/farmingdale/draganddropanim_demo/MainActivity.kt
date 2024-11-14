b3f90b537a39c8d1946a26e8df739d35e83bf70a
OsmanyARdx <osmarguet@gmail.com>
2024-11-14 14:33:59 -0500

    Complete TODO #5

--- app/src/main/java/edu/farmingdale/draganddropanim_demo/MainActivity.kt
+++ app/src/main/java/edu/farmingdale/draganddropanim_demo/MainActivity.kt
@@ -1,9 +1,11 @@
 package edu.farmingdale.draganddropanim_demo
 
+import android.content.pm.ActivityInfo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.gestures.Orientation
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -16,6 +18,7 @@ import edu.farmingdale.draganddropanim_demo.ui.theme.DragAndDropAnim_DemoTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
         enableEdgeToEdge()
         setContent {
             DragAndDropAnim_DemoTheme {
